de8e4327f789063a3230dc8a1e00c319bc19c10e
Fahmi-HI <fahmiiqtishom12@gmail.com>
2026-03-01 15:54:56 +0700

    Update RunSession.h/.cpp

--- src/RunSession.cpp
+++ src/RunSession.cpp
@@ -2,5 +2,21 @@
 #include <iostream>
 
 void RunSession::start() {
-    std::cout << "Run started\n";
+    ScoringSystem scoring;
+    ShopSystem shop;
+    std::vector<IModifier*> modifiers;
+
+    for (int round = 1; round <= 3; round++) {
+        std::cout << "\n=== Round " << round << " ===\n";
+
+        int baseScore = 10 * round;
+        std::cout << "Base Score: " << baseScore << "\n";
+
+        int finalScore = scoring.calculateScore(baseScore, modifiers);
+        std::cout << "Final Score: " << finalScore << "\n";
+
+        shop.openShop(modifiers);
+    }
+
+    for (auto m : modifiers) delete m;
 }
--- src/RunSession.h
+++ src/RunSession.h
@@ -1,6 +1,11 @@
 #ifndef RUN_SESSION_H
 #define RUN_SESSION_H
 
+#include <vector>
+#include "modifiers/IModifier.h"
+#include "ScoringSystem.h"
+#include "ShopSystem.h"
+
 class RunSession {
 public:
     void start();
