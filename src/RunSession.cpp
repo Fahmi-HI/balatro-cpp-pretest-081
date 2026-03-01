5b5c2d67caa669e46ee40d8ab541e39f6312a335
Fahmi-HI <fahmiiqtishom12@gmail.com>
2026-03-01 16:18:45 +0700

    implement random base score on RunSession.cpp with active modifier display

--- src/RunSession.cpp
+++ src/RunSession.cpp
@@ -1,15 +1,29 @@
 #include "RunSession.h"
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 void RunSession::start() {
     ScoringSystem scoring;
     ShopSystem shop;
     std::vector<IModifier*> modifiers;
 
+    srand(time(nullptr)); 
+
     for (int round = 1; round <= 3; round++) {
         std::cout << "\n=== Round " << round << " ===\n";
 
-        int baseScore = 10 * round;
+        std::cout << "Active Modifiers:\n";
+        if (modifiers.empty()) {
+            std::cout << "- None\n";
+        } else {
+            for (auto modifier : modifiers) {
+                std::cout << "- " << modifier->getName() << "\n";
+            }
+        }
+
+        int baseScore = (rand() % 5 + 1) * 10;
+        std::cout << "You played a hand.\n";
         std::cout << "Base Score: " << baseScore << "\n";
 
         int finalScore = scoring.calculateScore(baseScore, modifiers);
@@ -18,5 +32,7 @@ void RunSession::start() {
         shop.openShop(modifiers);
     }
 
-    for (auto m : modifiers) delete m;
+    for (auto m : modifiers) {
+        delete m;
+    }
 }
